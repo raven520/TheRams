823b39d82c4c60db4908868bbedec3b101ab99a2
Administrator <administrator@I46241P64126159.local>
2019-07-29 11:37:17 -0500

    add missing files

---
+++ urself/RegistrationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegistrationViewController.swift
+//  urself
+//
+//  Created by Administrator on 7/29/19.
+//  Copyright © 2019 Administrator. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegistrationViewController: UIViewController{
+    
+    var firstName: String = ""
+    var lastName: String = ""
+    var email: String = ""
+    var password: String = ""
+    var confirmPass: String = ""
+    
+    
+    
+}
