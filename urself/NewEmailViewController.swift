bc028f1015f043d8b6d55055a7e1836d50172477
Emily <emphan@cps.edu>
2019-07-30 11:01:15 -0500

    email controller

--- urself/NewEmailViewController.swift
+++ urself/NewEmailViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 
 class NewEmailViewController: UIViewController {
     
-    var olemail: String = ""
-    @IBOutlet weak var olemailTextField : UITextField!
-    @IBAction func olemailData(_ sender: Any){
+    var oemail: String = ""
+    @IBOutlet weak var oemailTextField : UITextField!
+    @IBAction func oemailData(_ sender: Any){
         oemail = oemailTextField.text ?? ""
     }
     
